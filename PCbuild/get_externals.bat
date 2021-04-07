python/cpython

bpo-43631: Update to OpenSSL 1.1.1k (GH-25024)
- [x] Build OpenSSL 1.1.1k for macOS
- [x] Build OpenSSL 1.1.1k for Windows

I have also updated multissl tester and various CI configurations to use latest OpenSSL. The versions were all over the place.

Signed-off-by: Christian Heimes <christian@python.org>

Automerge-Triggered-By: GH:tiran

PCbuild/get_externals.bat
@@ -53,7 +53,7 @@ echo.Fetching external libraries...
 set libraries=
 set libraries=%libraries%                                       bzip2-1.0.6
 if NOT "%IncludeLibffiSrc%"=="false" set libraries=%libraries%  libffi
-if NOT "%IncludeSSLSrc%"=="false" set libraries=%libraries%     openssl-1.1.1i
+if NOT "%IncludeSSLSrc%"=="false" set libraries=%libraries%     openssl-1.1.1k
 set libraries=%libraries%                                       sqlite-3.34.0.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tcl-core-8.6.10.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tk-8.6.10.0
@@ -77,7 +77,7 @@ echo.Fetching external binaries...
 
 set binaries=
 if NOT "%IncludeLibffi%"=="false"  set binaries=%binaries% libffi
-if NOT "%IncludeSSL%"=="false"     set binaries=%binaries% openssl-bin-1.1.1i
+if NOT "%IncludeSSL%"=="false"     set binaries=%binaries% openssl-bin-1.1.1k
 if NOT "%IncludeTkinter%"=="false" set binaries=%binaries% tcltk-8.6.10.0
 if NOT "%IncludeSSLSrc%"=="false"  set binaries=%binaries% nasm-2.11.06
 

OlCe2/tauthon

(cherry-picked from a54fc683f237d8f0b6e999a63aa9b8c0a45b7fef)
Removed irrelevant parts, minor changes in multissltests.py.
Co-authored-by: Olivier Certner <olce.tauthon@certner.fr>

PCbuild/get_externals.bat
@@ -47,7 +47,7 @@ rem files in both this dir and PC\VS9.0
 set libraries=
 set libraries=%libraries%                                    bzip2-1.0.6
 if NOT "%IncludeBsddb%"=="false" set libraries=%libraries%   bsddb-4.7.25.0
-if NOT "%IncludeSSL%"=="false" set libraries=%libraries%     openssl-1.0.2t
+if NOT "%IncludeSSL%"=="false" set libraries=%libraries%     openssl-1.1.1k
 set libraries=%libraries%                                    sqlite-3.28.0.0
 if NOT "%IncludeTkinter%"=="false" set libraries=%libraries% tcl-8.5.19.0
 if NOT "%IncludeTkinter%"=="false" set libraries=%libraries% tk-8.5.19.0